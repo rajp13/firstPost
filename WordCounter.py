6e77b77159a45d825eff1978e0e7d37929b96cd1
rajp13 <47237614+rajp13@users.noreply.github.com>
2019-02-13 15:46:49 -0600

    fixed some spelling errors in my comments

--- WordCounter.py
+++ WordCounter.py
@@ -2,13 +2,13 @@ import string #import string from the python library.
 def wordcount(filename): #takes a file.txt
     infile=open(filename) #open the file
     content=infile.read() #read the contents in the file
-    #erasePunctuation will clean all the punctions like commas, periods, etc. and replace them with spaces
+    #erasePunctuation will clean all the punctuations like commas, periods, etc. and replace them with spaces
     erasePunctuation=str.maketrans(string.punctuation+string.digits,' '*len(string.punctuation+string.digits)) 
     cleanTxt=content.translate(erasePunctuation)
     wordDict={} #emtpy dictionary
-    for word in cleanTxt.split(): #for words in cleantxt that are split
+    for word in cleanTxt.split(): #for words in cleanTxt that are split
         if word not in wordDict.keys():
-            wordCount=content.count(word)#counts teh number of words
-            wordDict[word]=wordCount #adds the word and there occurences to the dictionary
+            wordCount=content.count(word)#counts the number of words
+            wordDict[word]=wordCount #adds the word and there occurrences to the dictionary
             print('{:20}{}'.format(word,wordCount))#prints the dictionary 
 
